7a338988cae905a3df3b019ae87572055edfeebf
Kertész Domokos <kerteszdomokos@gmail.com>
2021-11-20 08:14:49 +0100

    Oras json adatgyűjtés
    Consumption/Flow time grafikonhoz
    311093 vízhasználat adat: nincs apartmanelemzés

--- backend/flask/algorithms.py
+++ backend/flask/algorithms.py
@@ -8,25 +8,24 @@ def oras():
     h=data["houses"]
     cons=[]
     flowt=[]
-    time=[]
+    timest=[]
     
-    print(h[0]["apartments"][0].keys())
+    print(len(h[0]["apartments"]))
     
     for i in range(20):
         keys=list(h[0]["apartments"][0].keys())
-        print (keys)
         x=1
-        print(h[i]["apartments"][0][keys[x]]["measurements"][0].keys())
-        print(h[i]["apartments"][0][keys[x]].keys())
-        print(i)
+        print(len(keys))
+
         while x<6:
-            cons.append(h[i]["apartments"][0][keys[x]]["measurements"][0]["Consumption"])#
-            flowt.append(h[i]["apartments"][0][keys[x]]["measurements"][0]["FlowTime"])
-            time.append(h[i]["apartments"][0][keys[x]]["measurements"][0]["TimeStamp"])
+            for y in h[0]["apartments"][i][keys[x]]["measurements"]:
+                cons.append(h[0]["apartments"][i][keys[x]]["measurements"][0]["Consumption"])#
+                flowt.append(h[0]["apartments"][i][keys[x]]["measurements"][0]["FlowTime"])
+                timest.append(h[0]["apartments"][i][keys[x]]["measurements"][0]["TimeStamp"])
             x=x+1
         
     # print(h[0]["apartments"][0]["Kitchen_optima_faucet"]["measurements"][0].keys())#
-    print (len(cons))
+    print (len(cons),len(flowt),len(timest))
     
     f.close()
 
